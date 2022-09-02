0f01d9ff621d508355a9584881c95a4c17a14dec
rajasijaywant3129 <rajasijaywant3129@gmail.com>
2022-09-02 15:20:11 +0530

    covid tracking

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from "./src/Config/Navigation"
+import CovidData from './src/covidData';
+const App = ()  => {
+  return (
+    <CovidData />
+  );
+};
+
+
+export default App;
+
+
